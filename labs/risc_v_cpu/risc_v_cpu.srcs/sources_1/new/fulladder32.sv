546c319a8053da1dbb2896d21b00a371d0bf25d7
Igor Markov <igor.markov@synchtank.net>
2025-03-13 21:10:44 +0100

    created one risc-v project for all labs

---
+++ labs/risc_v_cpu/risc_v_cpu.srcs/sources_1/new/fulladder32.sv
@@ -0,0 +1,17 @@
+module fulladder32(
+    input  logic [31:0] a_i,
+    input  logic [31:0] b_i,
+    input  logic        carry_i,
+    output logic [31:0] sum_o,
+    output logic        carry_o
+);
+    logic [30:0] carry;
+    
+    fulladder fa [31:0] (
+        .a_i(a_i),
+        .b_i(b_i),
+        .carry_i({carry[30:0], carry_i}),
+        .sum_o(sum_o),
+        .carry_o({carry_o, carry[30:0]})
+    );
+endmodule
